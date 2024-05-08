e32a81635266c38c615d4e8e900cde4565a765b9
unknown <ancjimenezm@udistrital.edu.co>
2024-05-07 21:17:47 -0500

    deploy test

--- src/main/java/com/groupc/fourparks/FourParksApplication.java
+++ src/main/java/com/groupc/fourparks/FourParksApplication.java
@@ -3,7 +3,6 @@ package com.groupc.fourparks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FourParksApplication {
     public static void main(String[] args) {
--- src/main/java/com/groupc/fourparks/infraestructure/config/SecurityConfig.java
+++ src/main/java/com/groupc/fourparks/infraestructure/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.groupc.fourparks.infraestructure.config;
 
-
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,14 +24,10 @@ import com.groupc.fourparks.infraestructure.config.jwt.JwtUtils;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
 
     private final JwtUtils jwtUtils;
-
-    public SecurityConfig(final JwtUtils jwtUtils) {
-        this.jwtUtils = jwtUtils;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
