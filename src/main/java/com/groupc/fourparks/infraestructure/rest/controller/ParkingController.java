097d7e25372ccec7d80eb05e6596b74a239d077e
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-05-10 14:43:51 -0500

    adding cross origin to front domain and email detail

--- src/main/java/com/groupc/fourparks/infraestructure/rest/controller/ParkingController.java
+++ src/main/java/com/groupc/fourparks/infraestructure/rest/controller/ParkingController.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.groupc.fourparks.application.service.ParkingServiceImpl;
 import com.groupc.fourparks.infraestructure.model.dto.ParkingDto;
@@ -18,14 +14,12 @@ import com.groupc.fourparks.infraestructure.model.request.SetAdminToParkingReque
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/parking")
+// @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://fourparks.vercel.app/")
 public class ParkingController {
     private final ParkingServiceImpl parkingServiceImpl;
 
